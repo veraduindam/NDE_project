9808354077fc132592cdf7cba9cbd9251571b168
Vera <duindamvera@gmail.com>
2022-05-11 11:22:23 +0200

    Time dependent solution

--- error_analysis.py
+++ error_analysis.py
@@ -1,4 +1,23 @@
-from project import FD_solver
+from project import FD_solver, true_sol, get_linear_index
+import numpy as np
 
-u = FD_solver(5, 5, 5)
-for 
+def error(Nx, Ny, Nz):
+    hx = 1/(Nx - 1)
+    hy = 1/(Ny - 1)
+    hz = 1/(Nz - 1)
+
+    u = FD_solver(Nx, Ny, Nz)
+    true_solution = np.zeros(Nx * Ny * Nz)
+    for i in range(Nx):
+        for j in range(Ny):
+            for k in range(Nz):
+                linear_index = get_linear_index(i, j, k, Nx, Ny)
+                true_solution[linear_index] = true_sol(i * hx, j * hy, k * hz)
+    
+    # for i in range(len(u)):
+    #     print(u[i], true_solution[i])
+    error = np.linalg.norm(u - true_solution, ord=np.inf)
+    print(f"Error: {error}")
+    return error
+
+error(10, 10, 10)
--- project.py
+++ project.py
@@ -1,12 +1,13 @@
 from decimal import DivisionByZero
 from math import exp, sin
 import numpy as np
-from solvers import conj_grad
+from solvers import conj_grad, jacobi_solver
 import itertools
 from export_utils import export_results
 
 
-# Nx, Ny, Nz - total number of nodes
+# Nx, Ny, Nz are the total number of nodes
+# returns list of the nodes that are not on the boundary
 def get_free_nodes_list(Nx, Ny, Nz):
     x = [i for i in range(1, Nx - 1)]
     y = [j for j in range(1, Ny - 1)]
@@ -15,12 +16,16 @@ def get_free_nodes_list(Nx, Ny, Nz):
     free_nodes = []
     product = itertools.product(*[x, y, z])
     for element in product:
-        print(element)
         free_nodes.append(get_linear_index(*element, Nx, Ny))
-    print(free_nodes)
     return free_nodes
 
 
+# get the index in the array of (i, j, k)
+def get_linear_index(i, j, k, N, M):
+    return (N * M) * k + N * j + i
+
+
+# go from the linear index back to the index (i, j, k)
 def get_3D_index(linear_index, N, M):
     i = linear_index % N
     k = linear_index // (N * M)
@@ -29,7 +34,7 @@ def get_3D_index(linear_index, N, M):
 
 
 # the true solution u
-def u(x, y, z):
+def true_sol(x, y, z):
     return exp(x) * sin(2 * y) + exp(y) * sin(float(z) / 2) + exp(z) * sin(2 * x)
 
 
@@ -38,18 +43,13 @@ def f(x, y, z):
     return 3 * exp(z) * sin(2 * x) + 3 * exp(x) * sin(2 * y) - float(3) / 4 * exp(y) * sin(float(z) / 2)
 
 
-# get the index in the array of 
-def get_linear_index(i, j, k, N, M):
-    return (N * M) * k + N * j + i
-
-
-# This gives us the matrices Dx, Dy and Dz
+# this gives us the matrices Dx, Dy and Dz
 def D(N):
     return np.diag([1] * (N - 1), -1) + np.diag([-2] * N, 0) + np.diag([1] * (N - 1), 1)
 
 
-# 
-def R_g_and_Z(Nx, Ny, Nz):
+# computation of R_g and the discrete vector F
+def R_g_and_F(Nx, Ny, Nz):
     hx = 1 / (Nx - 1)
     hy = 1 / (Ny - 1)
     hz = 1 / (Nz - 1)
@@ -64,11 +64,12 @@ def R_g_and_Z(Nx, Ny, Nz):
                 F[index] = f(i * hx, j * hy, k * hz)
 
                 if i == 0 or j == 0 or k == 0 or i == Nx - 1 or j == Ny - 1 or k == Nz - 1:
-                    R_g[index] = u(i * hx, j * hy, k * hz)
+                    R_g[index] = true_sol(i * hx, j * hy, k * hz)
 
-    return R_g, F - R_g
+    return R_g, F
 
 
+# solving the problem
 def FD_solver(Nx, Ny, Nz):
     # defining the grid
     hx = 1 / (Nx - 1)
@@ -98,27 +99,28 @@ def FD_solver(Nx, Ny, Nz):
     # define matrix A
     A = Kx + Ky + Kz
 
-    R_g, z = R_g_and_Z(Nx, Ny, Nz)
-    
+    # define vectors R_g, F and z
+    R_g, F = R_g_and_F(Nx, Ny, Nz)
+    z = F - A @ R_g
+
+    # take the part of the system that is still free and solve
     fd = get_free_nodes_list(Nx, Ny, Nz)
-    for node in fd:
-        print(node)
-        print(get_3D_index(node, Nx, Ny))
     A_free = A[:, fd][fd, :]
     z_free = z[fd]
-    v_free = conj_grad(A_free, z_free, 25)
+    v_free = jacobi_solver(A_free, z_free, 25)
 
+    # create v in correct dimension
     v = np.zeros((Nx * Ny * Nz))
     for i in range((Nx - 2) * (Ny - 2) * (Nz - 2)):
         v[fd[i]] = v_free[i]
 
+    # define solution u
     u = R_g + v
 
-    print(f"U = {u}")
     return u
-    # print(A)
 
 
+# function needed for plotting the solution: 
 def convert_to_3D(u, Nx, Ny, Nz):
     tensor = np.zeros((Nx, Ny, Nz))
     for l in range(len(u)):
